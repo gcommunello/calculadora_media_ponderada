fa4c24d76d5dfdddaae15db5300f75b71504d97a
Gabe <121589145+gcommunello@users.noreply.github.com>
2023-11-15 10:46:42 -0300

    Add files via upload

---
+++ Calculadora_UniSENAI.py
@@ -0,0 +1,16 @@
+print("Insira a primeira nota")
+nota1 = float(input())
+print("Insira a segunda nota")
+nota2 = float(input())
+print("Insira a terceira nota")
+nota3 = float(input())
+
+print("Insira o peso da primeira nota")
+peso1 = int(input())
+print("Insira o peso da segunda nota")
+peso2 = int(input())
+print("Insira o peso da terceira nota")
+peso3 = int(input())
+
+media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / 10
+print(media)
